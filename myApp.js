5f690cc9a62ee80be1a90d80aaa8bd008348d88d
John Albright <John@johnalbightsMBP.attlocal.net>
2021-08-17 04:00:17 -0500

    Connected project to MongoDB Atlas

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 let Person;
 
@@ -9,7 +14,7 @@ const createAndSavePerson = (done) => {
 
 const createManyPeople = (arrayOfPeople, done) => {
   done(null /*, data*/);
-};
+};a
 
 const findPeopleByName = (personName, done) => {
   done(null /*, data*/);
